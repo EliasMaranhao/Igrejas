1f62f774cf8ac347db08429d232f4ee0f523cbed
unknown <elias.ma.antonio@gmail.com>
2023-01-05 11:27:11 -0300

    Igrejas

--- visitantes/admin.py
+++ visitantes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Visitante
 
 # Register your models here.
+
+class ListandoVisitantes(admin.ModelAdmin):
+    list_display = ('nome', 'igreja_origem')
+    list_display_links = ('nome',)
+    search_fields = ('nome', 'data_visita')
+    list_filter = ('evangelico', 'primeira_vez')
+    list_per_page = 5
+    
+
+admin.site.register(Visitante, ListandoVisitantes)
--- visitantes/models.py
+++ visitantes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Visitante(models.Model):
+
+    nome = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, blank=True)
+    data_visita = models.DateTimeField(default=datetime.now())
+    evangelico = models.BooleanField(default=False)
+    primeira_vez = models.BooleanField(default=True)
+    igreja_origem = models.CharField(max_length=200, default='Não pertence a nenhuma igreja')
+    pastor = models.CharField(max_length=200, blank=True)
+    observacao = models.TextField(max_length=1000, blank=True)
